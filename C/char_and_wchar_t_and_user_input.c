805d6edf2a77612f49dc53d60b2cb8784486ff68
pufithenoob <pufithenoob@gmail.com>
2023-05-21 23:38:40 +0200

    Add printf's

--- C/char_and_wchar_t_and_user_input.c
+++ C/char_and_wchar_t_and_user_input.c
@@ -7,31 +7,41 @@
 
 
 #include <stdio.h>
+#include <wchar.h>
 
 int main(void) {
 
     // char (1 byte)    
-    char character;
+    const char STANDARD_HARD_CODED_CHARACTER = 'ф';
+    char standard_input_character;
 
-    printf("Enter a normal character: ");
-    character = fgetc(stdin);
+    printf("Enter a character from UTF-16 or bigger: ");
+    standard_input_character = fgetc(stdin);
     fseek(stdin, 0, SEEK_END);
 
-    printf("%c\n", character);
+    printf("STANDARD_HARD_CODED_CHARACTER as %%c=%c\n", STANDARD_HARD_CODED_CHARACTER);
+    printf("STANDARD_HARD_CODED_CHARACTER as %%d=%d\n", STANDARD_HARD_CODED_CHARACTER);
+    
+    printf("standard_input_character as %%c=%c\n", standard_input_character);
+    printf("standard_input_character as %%d=%d\n\n\n", standard_input_character);
 
 
 
 
 
     // wchar_t (4 byte)
-    char character;
-    printf("Enter a normal character: ");
-    character = fgetc(stdin);
+    const wchar_t WIDE_HARD_CODED_CHARACTER = 'ф';
+    wchar_t wide_input_character;
+
+    printf("Enter a character from UTF-16 or bigger: ");
+    wide_input_character = fgetc(stdin);
     fseek(stdin, 0, SEEK_END);
 
-    printf("%c\n", character);
-
-
+    printf("WIDE_HARD_CODED_CHARACTER as %%lс=%lс\n", WIDE_HARD_CODED_CHARACTER);
+    printf("WIDE_HARD_CODED_CHARACTER as %%d=%d\n", WIDE_HARD_CODED_CHARACTER);
+    
+    printf("wide_input_character as %%ls=%ls\n", wide_input_character);
+    printf("wide_input_character as %%d=%d\n", wide_input_character);
 
     return 0;
 
